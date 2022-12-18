380977f23cd05374ad4fa2ac18c671370cfcd9e9
Alexander Gerstberger <a.gerstberger@web.de>
2022-12-18 21:12:00 +0100

    organising code into modules

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,11 @@
+use server::Server;
+
+mod server;
+mod http;
+
 fn main() {
     let server = Server::new("localhost:8080".to_string());
 
     server.run();
 }
 
-struct Server {
-    addr: String,
-}
-
-impl Server {
-    fn new(addr: String) -> Self {
-        Self {
-            addr,
-        }
-    }
-
-    fn run(self) {
-        println!("Listening on {}", self.addr);
-    }
-}
-
-struct Request {
-    method: Method,
-    path: String,
-    query_string: Option<String>
-}
-
-enum Method {
-    GET,
-    POST,
-    PUT,
-    DELETE,
-    HEAD,
-    OPTIONS,
-    TRACE,
-    PATCH,
-    CONNECT,
-}
