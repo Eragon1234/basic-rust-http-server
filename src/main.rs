810c2b332de570474c3280ee941749bce650bf66
Alexander Gerstberger <a.gerstberger@web.de>
2022-12-18 18:32:54 +0100

    adding basic Server struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let server = Server::new("localhost:8080".to_string());
+
+    server.run();
+}
+
+struct Server {
+    addr: String,
+}
+
+impl Server {
+    fn new(addr: String) -> Self {
+        Self {
+            addr,
+        }
+    }
+
+    fn run(self) {
+        println!("Listening on {}", self.addr);
+    }
 }
