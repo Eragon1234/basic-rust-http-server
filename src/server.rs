7b4a6899474f03d9d04f4ec17141916e06987d3a
Alexander Gerstberger <a.gerstberger@web.de>
2022-12-19 14:08:57 +0100

    logging incoming requests to the console

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,4 @@
-use std::io::{BufReader, Read};
+use std::io::Read;
 use std::net::TcpListener;
 
 pub struct Server {
@@ -18,8 +18,17 @@ impl Server {
         let listener = TcpListener::bind(&self.addr).unwrap();
         loop {
             match listener.accept() {
-                Ok((stream, _)) => {
-                    println!("Connection from {}", stream.peer_addr().unwrap());
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+
+                    match stream.read(&mut buffer) {
+                        Ok(..) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+                        }
+                        Err(e) => {
+                            println!("Error: {}", e);
+                        }
+                    }
                 }
                 Err(e) => {
                     println!("Error: {}", e);
